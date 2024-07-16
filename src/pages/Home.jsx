c824e2bcea5d5ecc2ec042bd2700cc015da2d39c
yksl <yukselkurtuldu9@gmail.com>
2024-07-16 13:07:44 +0300

    up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
   const getTodo = async () => {
     try {
-      const res = await axios("https://todo-backend-4b9m.onrender.com");
+      const res = await axios("https://todo-backend-1-qayo.onrender.com/");
       console.log(res.data.result.rows);
       setTodos(res.data.result.rows);
     } catch (error) {
@@ -19,7 +19,7 @@ const Home = () => {
 
   const delTodo = async (id) => {
     try {
-      await axios.delete(`https://todo-backend-4b9m.onrender.com/${id}`);
+      await axios.delete(`https://todo-backend-1-qayo.onrender.com/${id}`);
       getTodo();
     } catch (error) {
       console.log(error);
@@ -28,7 +28,7 @@ const Home = () => {
 
   const postTodo = async (data) => {
     try {
-      await axios.post("https://todo-backend-4b9m.onrender.com", data);
+      await axios.post("https://todo-backend-1-qayo.onrender.com/", data);
       getTodo();
     } catch (error) {
       console.error(error);
@@ -37,7 +37,7 @@ const Home = () => {
 
   const putTodo = async (id, data) => {
     try {
-      await axios.put(`https://todo-backend-4b9m.onrender.com/${id}`, data);
+      await axios.put(`https://todo-backend-1-qayo.onrender.com/${id}`, data);
       getTodo();
     } catch (error) {
       console.error(error);
